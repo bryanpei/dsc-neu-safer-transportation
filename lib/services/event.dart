cd86d382ab6f9f739c453a341b0a0a26e8b807f6
Bryan <xbryanpei@gmail.com>
2020-02-11 13:38:54 -0800

    New commit with gitignore.

---
+++ lib/services/event.dart
@@ -0,0 +1,30 @@
+// Event class that stores all related
+// information of a single event.
+
+import 'package:safer_transportation/services/event_type.dart';
+import 'package:safer_transportation/services/event_source.dart';
+
+class Event {
+  final int intEventId;
+  final String datetime;
+  final String locationH; // human readable address.
+  final String imageUrl; // If there a image related to the event.
+  final dynamic locationGeo; // geo location.
+  final int reportUserId;  // could be null if event is not reported by user.
+  final EventSource source;
+  final EventType type;
+
+  Event({
+    this.intEventId,
+    this.datetime,
+    this.locationH,
+    this.imageUrl,
+    this.locationGeo,
+    this.reportUserId,
+    this.source,
+    this.type,
+  });
+
+  void updateEvent() {}
+
+}
