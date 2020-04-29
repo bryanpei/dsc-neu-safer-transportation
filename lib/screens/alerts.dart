9e05e38b64f1d443dde694c79ab5964188a132d9
Antoinette Cheryl <apcheryl@gmail.com>
2020-04-28 20:28:58 -0700

    Implemented 1 checkbox

--- lib/screens/alerts.dart
+++ lib/screens/alerts.dart
@@ -1,49 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:safer_transportation/components/menu.dart';
 
-class Alerts extends StatelessWidget {
+class Alerts extends StatefulWidget {
 
+  @override
+  _AlertsState createState() => _AlertsState();
+}
 
-//  @override
-//  _AlertsState createState() => _AlertsState();
-//}
-//
-//class _AlertsState extends State<Alerts> {
+class _AlertsState extends State {
 
-
+  bool alertControl = false;
+  bool checkBoxValue = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text('Alerts',
-          style: TextStyle(
-              color: Colors.black
-          ),
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.black54,
-          ),
-          bottom: PreferredSize(
-            child: Container(
-                color: Colors.grey, height: 2.0),
-          preferredSize: Size.fromHeight(2.0),
-        ),
-      ),
-        drawer: Menu(),
-        body: SafeArea(
-          child: Center(
-            child: SwitchWidget()
-          )
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              title: Text('Alerts',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                ),
+              ),
+              iconTheme: IconThemeData(
+                color: Colors.black54,
+              ),
+              bottom: PreferredSize(
+                child: Container(
+                    color: Colors.grey, height: 2.0),
+                preferredSize: Size.fromHeight(2.0),
+              ),
+            ),
+            drawer: Menu(),
+            body: Center(
+                child: Column(children: <Widget>[
+                  // The header, which includes Alerts and toggle switch
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        // The header: Alerts
+                        Container(
+                          margin: EdgeInsets.all(20.0),
+                          child: Text('Alerts', style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.black
+                          )),
+                        ),
+                        // The header: Toggle switch
+                        Container(
+                          child: SwitchWidget(),
+                        )
+                      ],
+                    ),
+                  ),
+                  // The alerts setting, which would be hidden when switch is OFF
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(right: 20.0),
+                      child: Text('Type', style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black54
+                      ),),
+                    ),
+                    Row( children: <Widget>[
+                      Checkbox(
+                        value: checkBoxValue,
+                        activeColor: Colors.black,
+                        onChanged: (bool value) {
+                          setState(() {
+                            checkBoxValue = value;
+                          });
+                        },
+                      ),
+                      Text('Syringe', style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.black
+                      ),)
+                    ],
+                    )
+                  ],)
+                ],)
+            )
         )
-    )
     );
   }
 }
-
-
 
 
 
@@ -65,7 +114,7 @@ class SwitchWidgetClass extends State {
         textHolder = 'Switch is ON';
       });
       print('Switch is ON');
-      // Code for when the switch is ON
+       // Code for when the switch is ON
 
 
 
@@ -87,11 +136,11 @@ class SwitchWidgetClass extends State {
       mainAxisAlignment: MainAxisAlignment.start,
         children: [Transform.scale(
             scale: 1.0,
-            child:  Switch(
+            child: Switch(
               onChanged: toggleSwitch,
               value: switchControl,
-              activeColor: Colors.blue,
-              activeTrackColor: Colors.lightBlue,
+              activeColor: Colors.blueAccent,
+              activeTrackColor: Colors.blueAccent,
               inactiveThumbColor: Colors.white,
               inactiveTrackColor: Colors.grey,
             )),
@@ -102,5 +151,21 @@ class SwitchWidgetClass extends State {
 //          )
     ]);
   }
+
+
+  // The checkboxes in the Alerts settings, which will be visible whenever
+  // toggle is switched to ON
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
